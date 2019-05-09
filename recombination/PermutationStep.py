81c28d7357ff4ced9377aba088e65ae4785d8b9a
paulaju <paula@schmiel.berlin>
2019-05-09 14:37:47 +0200

    docstrings

--- recombination/PermutationStep.py
+++ recombination/PermutationStep.py
@@ -1,6 +1,23 @@
 
 class PermutationStep:
 
+    """
+    Step of the permutation algorithm representing one single dummy atom of a molecule
+
+    Attributes
+    ----------
+    fragment: RDKit Mol object
+
+    dummy: RDKit Atom object
+        A dummy atom of the fragment
+
+    depth: int
+        Number of original fragments that this fragment consists of
+    subpockets: list(string)
+        List of subpockets that the fragment is targeting
+
+    """
+
     def __init__(self, fragment, dummy, depth, subpockets):
 
         self.fragment = fragment
