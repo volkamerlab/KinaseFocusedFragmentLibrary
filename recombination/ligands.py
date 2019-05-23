4f7f2d276e760590d5c9ca25e65241b5cb1da2dc
paulaju <paula@schmiel.berlin>
2019-05-23 14:13:09 +0200

    construct ligands from metadata

--- recombination/ligands.py
+++ recombination/ligands.py
@@ -7,32 +7,13 @@ Chem.SetDefaultPickleProperties(Chem.PropertyPickleOptions.AllProps)
 from pathlib import Path
 import pickle
 import sys
+import time
 sys.path.append("../fragmentation/")
 
+from pickle_loader import pickle_loader
 
-# ============================= FUNCTIONS ===============================================
+start = time.time()
 
-def pickle_loader(pickle_file):
-
-    """
-    Load a pickle file with multiple objects
-
-    Parameters
-    ----------
-    pickle_file: file object
-        input binary pickle file
-
-    Returns
-    -------
-    Generator object with loaded objects
-
-    """
-
-    try:
-        while True:
-            yield pickle.load(pickle_file)
-    except EOFError:
-        pass
 
 # ============================= READ FRAGMENT ===============================================
 
@@ -50,7 +31,7 @@ for folder, subpocket in zip(folders, subpockets):
     # read molecules
     # keep hydrogen atoms
     suppl = Chem.SDMolSupplier(str(file), removeHs=False)
-    mols = [f for f in suppl]
+    mols = [f for f in suppl][:20]
 
     fragments = []
     for i, fragment in enumerate(mols):
@@ -78,6 +59,8 @@ for folder, subpocket in zip(folders, subpockets):
 in_path = Path('meta_library.pickle')
 pickle_in = in_path.open('rb')
 
+#ligands = []
+ligand_smiles = set()
 # iterate over ligands
 for meta in pickle_loader(pickle_in):
 
@@ -100,9 +83,43 @@ for meta in pickle_loader(pickle_in):
         fragment = combo
         i += 1
 
+    bonds_matching = True
     ed_combo = Chem.EditableMol(combo)
-    #for bond in bonds:
-    #    dummy_1 =
-    #    ed_combo.AddBond(atom_1.GetIdx(), atom_2.GetIdx(), order=bond_type_1)
+    for bond in bonds:
+
+        dummy_1 = [atom for atom in combo.GetAtoms() if atom.GetProp('frag_atom_id') == bond[0]][0]
+        dummy_2 = [atom for atom in combo.GetAtoms() if atom.GetProp('frag_atom_id') == bond[1]][0]
+        atom_1 = dummy_1.GetNeighbors()[0]
+        atom_2 = dummy_2.GetNeighbors()[0]
+
+        # check bond types
+        bond_type_1 = combo.GetBondBetweenAtoms(dummy_1.GetIdx(), atom_1.GetIdx()).GetBondType()
+        bond_type_2 = combo.GetBondBetweenAtoms(dummy_2.GetIdx(), atom_2.GetIdx()).GetBondType()
+        if bond_type_1 != bond_type_2:
+            bonds_matching = False
+            break
+
+        ed_combo.AddBond(atom_1.GetIdx(), atom_2.GetIdx(), order=bond_type_1)
+
+    dummy_atoms = [a.GetIdx() for a in combo.GetAtoms() if a.GetSymbol() == '*']
+    dummy_atoms.sort(reverse=True)
+    for dummy in dummy_atoms:
+        ed_combo.RemoveAtom(dummy)
+
+    ligand = ed_combo.GetMol()
+
+    # do not construct this ligand if bond types are not matching
+    if not bonds_matching:
+        continue
+
+    smiles = Chem.MolToSmiles(ligand)
+    #if smiles in ligand_smiles:
+    #    continue
+
+    #ligands.append(ligand)
+    ligand_smiles.add(smiles)
 
 
+runtime = time.time() - start
+print('Number of resulting ligands: ', len(ligand_smiles))
+print('Time: ', runtime)
