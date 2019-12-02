71af9cd92abaeffb806503588e0928200ad68902
paulaju <paula@schmiel.berlin>
2019-12-02 13:07:11 +0100

    try-except ligand analysis

--- kinase_focused_fragment_library/analysis/ligand_analysis/analyze_results.py
+++ kinase_focused_fragment_library/analysis/ligand_analysis/analyze_results.py
@@ -8,48 +8,56 @@ from standardize import standardize_mol
 
 def analyze_result(meta, data, original_ligands, chembl):
 
-    ligand = construct_ligand(meta, data)
-    # if ligand could not be constructed, skip
-    if not ligand:
-        print('Ligand could not be constructed: ', meta.frag_ids)
+    try:
+
+        ligand = construct_ligand(meta, data)
+        # if ligand could not be constructed, skip
+        if not ligand:
+            print('ERROR: Ligand could not be constructed:', meta.frag_ids)
+            return
+
+        # necessary for Lipinski rule
+        # Chem.GetSymmSSSR(ligand)
+
+        ligand = standardize_mol(ligand)
+        # if ligand could not be standardized, skip
+        if not ligand:
+            print('ERROR: Ligand could not be standardized:', meta.frag_ids)
+            return
+
+        inchi = Chem.MolToInchi(ligand)
+
+        # Lipinski rule
+        lipinski, wt, logp, hbd, hba = is_drug_like(ligand)
+
+        # number of atoms
+        n = ligand.GetNumHeavyAtoms()
+
+        # search in original ligands
+        original = 0
+        original_sub = 0
+
+        # exact match in original ligand
+        if not original_ligands[original_ligands.inchi == inchi].empty:
+            original = 1
+
+        # true substructure of original ligands?
+        elif not original_ligands[original_ligands.mol >= ligand].empty:
+            original_sub = 1
+
+        # exact chembl match
+        chembl_match = 0
+        chembl_matches = chembl[chembl.standard_inchi == inchi]
+        if not chembl_matches.empty:
+            chembl_match = 1
+
+        # construct Result object
+        result = Result(meta, lipinski, wt, logp, hbd, hba, n, original, original_sub, chembl_match)
+
+        return result
+
+    except Exception as e:
+
+        print(e)
+        print('ERROR:', meta.frag_ids)
         return
-
-    # necessary for Lipinski rule
-    # Chem.GetSymmSSSR(ligand)
-
-    ligand = standardize_mol(ligand)
-    # if ligand could not be standardized, skip
-    if not ligand:
-        print('Ligand could not be standardized: ', meta.frag_ids)
-        return
-
-    inchi = Chem.MolToInchi(ligand)
-
-    # Lipinski rule
-    lipinski, wt, logp, hbd, hba = is_drug_like(ligand)
-
-    # number of atoms
-    n = ligand.GetNumHeavyAtoms()
-
-    # search in original ligands
-    original = 0
-    original_sub = 0
-
-    # exact match in original ligand
-    if not original_ligands[original_ligands.inchi == inchi].empty:
-        original = 1
-
-    # true substructure of original ligands?
-    elif not original_ligands[original_ligands.mol >= ligand].empty:
-        original_sub = 1
-
-    # exact chembl match
-    chembl_match = 0
-    chembl_matches = chembl[chembl.standard_inchi == inchi]
-    if not chembl_matches.empty:
-        chembl_match = 1
-
-    # construct Result object
-    result = Result(meta, lipinski, wt, logp, hbd, hba, n, original, original_sub, chembl_match)
-
-    return result
