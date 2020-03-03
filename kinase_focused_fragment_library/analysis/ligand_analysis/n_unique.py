7b581831e9d7ca64434aa9e12f153f2a2d1a9814
dominiquesydow <dominique.sydow@posteo.de>
2020-03-03 11:13:35 +0100

    Refactor script (get unique recombined molecule ids/number)

--- kinase_focused_fragment_library/analysis/ligand_analysis/n_unique.py
+++ kinase_focused_fragment_library/analysis/ligand_analysis/n_unique.py
@@ -1,72 +1,105 @@
-from rdkit import Chem
-from rdkit.Chem import Draw
-from rdkit.Chem.Draw import IPythonConsole
-from rdkit.Chem.PropertyMol import PropertyMol
-Chem.SetDefaultPickleProperties(Chem.PropertyPickleOptions.AllProps)
-import pickle
-import sys
-from pathlib import Path
-import pandas as pd
-import numpy as np
+"""
+Get indices (and total number) of unique recombined molecules.
+To be run on a cluster!
 
-from construct_ligand import construct_ligand, read_fragment_library
-from standardize import standardize_mol
+At the moment: Needs to be run from folder:
+/kinase_focused_fragment_library/analysis/ligand_analysis/
+"""
+
+from pathlib import Path
+import sys
+
+import numpy as np
+import pandas as pd
+from rdkit import Chem
+
+from kinase_focused_fragment_library.analysis.ligand_analysis.construct_ligand import construct_ligand, read_fragment_library
+from kinase_focused_fragment_library.analysis.ligand_analysis.standardize import standardize_mol
+
+"""
+At the moment:
+We will need to add the path to the Results class because the pickled object 'combinatorial_library' was
+constructed before KinaseFocusedFragmentLibrary became a package and does not know where to search for the class.
+Responsible file (that needs to be changed to package-style imports):
+/kinase_focused_fragment_library/analysis/ligand_analysis/analyze_results.py
+
+Then the recombination would need to be rerun - and then one could remove the sys statements and use this 
+path-independent import:
+from kinase_focused_fragment_library.recombination.pickle_loader import pickle_loader
+"""
+
 sys.path.append('../../recombination')
 from pickle_loader import pickle_loader
 
+# Set default pickle properties for rdkit
+Chem.SetDefaultPickleProperties(Chem.PropertyPickleOptions.AllProps)
+
+# set IO folders
+data_path = Path('/home/dominique/Documents/Projects/KinaseFocusedFragmentLibraryData')
+fragment_library_path = data_path / 'FragmentLibrary'
+combinatorial_library_path = data_path / 'CombinatorialLibrary'
+
+# set output paths
+combinatorial_library_file = combinatorial_library_path / 'combinatorial_library.pickle'
+output_unique_molecule_number_file = combinatorial_library_path / 'unique_molecule_number.txt'
+output_unique_molecule_ids_file = combinatorial_library_path / 'unique_molecule_ids.txt'
+
 # load fragments (reduced library)
 subpockets = ['AP', 'FP', 'SE', 'GA', 'B1', 'B2']
-data = read_fragment_library(Path('/home/paula/Masterarbeit/FragmentLibrary'), subpockets)
+fragment_library = read_fragment_library(fragment_library_path, subpockets)
 
-# combinatorial library
-combinatorial_library_folder = Path('/home/paula/Masterarbeit/CombinatorialLibrary/')
-file_name = combinatorial_library_folder / 'combinatorial_library.pickle'
-
-out_file_name = combinatorial_library_folder / 'unique.txt'
-
-# create empty dataframe to store inchis (length=number of recombinations)
-all_inchis = pd.Series('', index=range(15881010))
-
+# create dictionary for molecule inchis and IDs
+molecule_inchis = {'index': [], 'inchi': []}
 
 # load recombinations 
-with open(file_name, 'rb') as pickle_file:
+with open(combinatorial_library_file, 'rb') as pickle_file:
 
-    for i, result in enumerate(pickle_loader(pickle_file)):
+    for molecule_index, result in enumerate(pickle_loader(pickle_file)):
         
         # print progress
-        if i % 100000 == 0:
-            print(i)
+        if molecule_index % 100000 == 0:
+            print(f'Progress: {molecule_index}')
         
         # construct molecule
         try: 
-            ligand = construct_ligand(result.meta, data)           
+            molecule = construct_ligand(result.meta, fragment_library)
         except Exception as e:
             print(e)
-            print('ERROR:', i, result.meta.frag_ids)
+            print(f'Error: Molecule construction failed: '
+                  f'Molecule index {molecule_index} with fragment IDs {result.meta.frag_ids}')
             continue
 
         # standardize molecule
         try:    
-            std_ligand = standardize_mol(ligand)
+            molecule_standardized = standardize_mol(molecule)
         except Exception as e:
             print(e)
-            print('ERROR:', i, result.meta.frag_ids)
+            print(f'Error: Molecule standardization failed: '
+                  f'Molecule index {molecule_index} with fragment IDs {result.meta.frag_ids}')
             continue
 
         # convert mol to inchi    
         try: 
-            inchi = Chem.MolToInchi(std_ligand)
+            molecule_inchi = Chem.MolToInchi(molecule_standardized)
         except Exception as e:
             print(e)
-            print('ERROR:', i, result.meta.frag_ids)
+            print(f'Error: Mol to inchi conversion failed: '
+                  f'Molecule index {molecule_index} with fragment IDs {result.meta.frag_ids}')
             continue
             
-        # store inchi in dataframe    
-        all_inchis[i] = inchi
+        # add inchi with molecule ID
+        molecule_inchis['index'].append(molecule_index)
+        molecule_inchis['inchi'].append(molecule_inchi)
 
-# drop duplicates
-unique_inchis_ids = list(all_inchis.drop_duplicates().index)
-print('Number of unique recombined molecules:', len(unique_inchis_ids))
+# create series
+molecule_inchis_df = pd.Series(molecule_inchis['inchi'], index=molecule_inchis['index'])
 
-# save indices of unique recombined molecules
-np.savetxt(out_file_name, unique_inchis_ids, fmt='%d')
+# unique molecule ids (drop inchi duplicates)
+unique_molecule_ids = list(molecule_inchis_df.drop_duplicates().index)
+
+# write to file: number of unique recombined molecules
+with open(output_unique_molecule_number_file, 'w') as f:
+    f.write(f'Number of unique recombined molecules: {len(unique_molecule_ids)}')
+
+# write to file: indices of unique recombined molecules
+np.savetxt(output_unique_molecule_ids_file, unique_molecule_ids, fmt='%d')
