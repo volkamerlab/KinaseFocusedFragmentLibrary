d6a733fa10a982d6cc9648fc132c130e3fd4bdc3
dominiquesydow <dominique.sydow@posteo.de>
2020-03-09 17:00:50 +0100

    Add utility module to fragment analysis

--- kinase_focused_fragment_library/analysis/__init__.py
+++ kinase_focused_fragment_library/analysis/__init__.py
@@ -4,4 +4,5 @@ Subpocket-based fragmentation of kinase inhibitors
 """
 
 # Add imports of sub-modules here
-from .ligand_analysis import *
+from .ligand_analysis import util
+from .fragment_analysis import util
