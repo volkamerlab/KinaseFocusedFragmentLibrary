1b13934c80edb22c21250f9c4c1a456de54ba44b
dominiquesydow <dominique.sydow@posteo.de>
2020-03-19 10:14:10 +0100

    Add drug likeness for deduplicated recomb. lib.

--- kinase_focused_fragment_library/analysis/ligand_analysis/util.py
+++ kinase_focused_fragment_library/analysis/ligand_analysis/util.py
@@ -2,12 +2,17 @@
 Utility functions to work with the combinatorial library.
 """
 
+from datetime import datetime
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import Descriptors, Lipinski
+
+import sys
+sys.path.append('../../recombination')  # Not pretty but pickle file can only be loaded with explicit paths
+from pickle_loader import pickle_loader
 
 
 def load_combinatorial_library_properties(recombined_ligand_properties_path, ligand_indices_deduplicated=None):
@@ -155,6 +160,77 @@ def drug_likeness_from_mols(mols):
     print(f'Number of molecules: {drug_likeness.shape[0]}')
 
     drug_likeness_ratio = round(drug_likeness.apply(sum) / len(drug_likeness) * 100, 0)
+
+    return drug_likeness_ratio
+
+
+def drug_likeness_from_pickle(combinatorial_library_path, ligand_ixs):
+    """
+    Get drug-likeness from pickle file containing recombinatorial library metadata.
+
+    Parameters
+    ----------
+    combinatorial_library_path : pathlib.Path
+        Path to pickle file containing combinatorial library.
+    ligand_ixs : None or list of int
+        If None, full library is processed (default). Alternatively, pass list of ligand indices.
+
+    Returns
+    -------
+    pd.Series
+        Ratio of drug like molecules.
+    """
+
+    combinatorial_library_path = Path(combinatorial_library_path)
+    
+    drug_likeness = {
+        'mw': 0,
+        'hba': 0,
+        'hbd': 0,
+        'logp': 0,
+        'lipinski': 0
+    }
+    
+    # get ligand indices of interest
+    ligand_ixs = sorted(ligand_ixs)
+    ligand_ix_max = ligand_ixs[-1]
+    ligand_ixs_n = len(ligand_ixs)
+
+    ligand_ixs = iter(ligand_ixs)
+    ligand_ix = next(ligand_ixs)
+      
+    print(f'{datetime.now()}: start')
+
+    with open(combinatorial_library_path, 'rb') as pickle_file:
+
+        for i, ligand in enumerate(pickle_loader(pickle_file)):
+
+            if i%1000000 == 0:
+                print(f'{datetime.now()}: step {i}')
+
+            if i == ligand_ix:
+                drug_likeness['mw'] += ligand.mwt
+                drug_likeness['hba'] += ligand.hba
+                drug_likeness['hbd'] += ligand.hbd
+                drug_likeness['logp'] += ligand.logp
+                drug_likeness['lipinski'] += ligand.lipinski
+
+                if ligand_ix < ligand_ix_max:
+                    ligand_ix = next(ligand_ixs)
+                else:
+                    # if maximum ligand index of interest is reached, 
+                    # stop iteration over pickle file
+                    break
+
+    print(f'{datetime.now()}: end')
+                    
+    drug_likeness = pd.Series(drug_likeness)
+                    
+    print(f'Number of molecules: {ligand_ixs_n}')
+    print(drug_likeness)
+    print(drug_likeness / ligand_ixs_n * 100)
+
+    drug_likeness_ratio = round(drug_likeness / ligand_ixs_n * 100, 0)
 
     return drug_likeness_ratio
 
