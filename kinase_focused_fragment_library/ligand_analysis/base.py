1463a11d2379a957cb08168aee4101072d0e0af7
Unknown <dominique.sydow@posteo.de>
2020-06-13 10:28:05 +0200

    Add base module and function skeletons

--- kinase_focused_fragment_library/ligand_analysis/base.py
+++ kinase_focused_fragment_library/ligand_analysis/base.py
@@ -5,3 +5,18 @@ Subpocket-based fragmentation of kinase inhibitors
 This module contains basic classes for the analysis of the combinatorial library.
 """
 
+
+class ChemblPreparer:
+    """
+    Class to prepare the raw ChEMBL dataset for the analysis of the combinatorial library: Load raw data,
+    """
+
+    def __init__(self, path_chembl_raw, path_chembl_out):
+        self.path_chembl_raw = path_chembl_raw
+        self.path_chembl_out = path_chembl_out
+
+
+class CombinatorialLibraryAnalyzer():
+
+    def __init__(self):
+        pass
